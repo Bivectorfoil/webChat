db3df1bbac00f881164f8186399a28d3d4f18897
Bivectorfoil <bivectorfoil@gmail.com>
2020-06-08 22:28:27 +0800

    Starting webSockert part...

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,6 +2,8 @@ class ChatController < ApplicationController
     def index
     end
     def show
+        @user = User.find(params[:id])
+        @messages = Message.all
     end
     def new
     end
