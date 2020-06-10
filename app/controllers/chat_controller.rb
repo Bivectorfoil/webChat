f354f5bed175c9e671cc657bd0c7deed64474467
Bivectorfoil <bivectorfoil@gmail.com>
2020-06-10 23:07:33 +0800

    link User and message, add websocket part, left message saving part unfinished

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,15 +6,29 @@ class ChatController < ApplicationController
     def show
         @user = User.find(params[:id])
         @messages = Message.all
+        @message = Message.new
     end
     def new
+        @message = Message.new
     end
     def edit
     end
     def update
     end
     def create
+        @message = Message.create(msg_params)
+        if @message.save
+            puts "message save"
+            ActionCable.server.broadcast "chat_channel",
+            content: @message.body
+        else
+            puts "message save failed"
+        end
     end
     def destroy
+    end
+    private
+    def msg_params
+        params.require(:message).permit(:body)
     end
 end
